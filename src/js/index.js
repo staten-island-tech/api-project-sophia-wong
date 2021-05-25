56f29f934ad388566934dbf066e268ebf69b8f8c
NOT-SWong <sophia.wong22@sitechhs.com>
2021-05-25 16:50:30 -0400

    Successful Get Channel

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "AIzaSyAd1aEIUom1P_LW6oxVJ7hKfLX0mNP2KrY";
+
+const requestOptions = {
+  method: 'GET',
+};
+
+fetch("https://www.googleapis.com/youtube/v3/channels?key=AIzaSyAd1aEIUom1P_LW6oxVJ7hKfLX0mNP2KrY&part=snippet,contentDetails,statistics&forUsername=Lilypichu&key=AIzaSyAd1aEIUom1P_LW6oxVJ7hKfLX0mNP2KrY")
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+fetch("https://www.googleapis.com/youtube/v3/channels?key=AIzaSyAd1aEIUom1P_LW6oxVJ7hKfLX0mNP2KrY&part=snippet,contentDetails,statistics&forUsername=Pokimane&key=AIzaSyAd1aEIUom1P_LW6oxVJ7hKfLX0mNP2KrY")
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
